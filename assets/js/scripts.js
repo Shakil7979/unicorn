ba17c611d17746aca951d2db44b8c5737d50f11e
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-11-20 16:30:54 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,9 +15,23 @@ $(document).ready(function(){
 		navText: ["<img src='assets/images/promotion/left.png'>","<img src='assets/images/promotion/right.png'>"]
 	});
 
-	$(document).on('click','.mobile_bars',function(){
-		$('.mobile_menu').slideToggle();
-	})
+	// $(document).on('click','.mobile_bars',function(){
+	// 	$('.mobile_menu').slideToggle();
+	// })
+ 
+	$('.mobile_bars').click(function (e) {
+		e.preventDefault();  
+	
+		$('.mobile_menu').slideToggle(1000); 
+	
+		const icon = $(this).find('i');
+
+		if (icon.hasClass('fa-bars')) {
+			icon.removeClass('fa-bars').addClass('fa-times');  
+		} else {
+			icon.removeClass('fa-times').addClass('fa-bars');
+		}
+	}); 
 
 
 
